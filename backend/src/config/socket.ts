c25521d03ef7b05de27751edec484a08d9bc1afb
Mehtaab <mehtaabaalam117@gmail.com>
2026-07-20 23:51:38 +0530

    credentials

--- backend/src/config/socket.ts
+++ backend/src/config/socket.ts
@@ -9,7 +9,8 @@ let io: SocketServer | null = null;
 export function initSocket(server: HttpServer) {
   io = new SocketServer(server, {
     cors: {
-      origin: process.env.FRONTEND_URL || "http://localhost:3000", // Matches your Next.js port
+      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      credentials: true, // Matches your Next.js port
       methods: ["GET", "POST"],
     },
   });
